517a44937ab45af863bdc3818e17942234c9e1d2
tertbyte <sachumusthu@gmail.com>
2026-09-06 01:22:01 +0530

    Protect frontend behind authentication gate

--- SkillTrace-Professional/src/main.jsx
+++ SkillTrace-Professional/src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {createRoot} from "react-dom/client";
 import App from "./App";
+import SecurityGate from "./SecurityGate";
 import "./styles.css";
-createRoot(document.getElementById("root")).render(<React.StrictMode><App/></React.StrictMode>);
+createRoot(document.getElementById("root")).render(<React.StrictMode><SecurityGate><App/></SecurityGate></React.StrictMode>);
