f407933b33c4d79b5f2f336043f7d55d226a0783
tertbyte <sachumusthu@gmail.com>
2026-09-09 12:31:13 +0530

    Enable minimal gamified AI analysis styling

--- SkillTrace-Professional/src/main.jsx
+++ SkillTrace-Professional/src/main.jsx
@@ -3,4 +3,5 @@ import {createRoot} from "react-dom/client";
 import App from "./App";
 import SecurityGate from "./SecurityGate";
 import "./styles.css";
+import "./ai-gamified.css";
 createRoot(document.getElementById("root")).render(<React.StrictMode><SecurityGate><App/></SecurityGate></React.StrictMode>);
